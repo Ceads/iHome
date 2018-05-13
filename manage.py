403872947cf2e45a2fd4b60cf32dc50f1873a4b8
Ceads <allenjo@163.com>
2018-05-13 20:42:59 +0800

    项目初始化&git管理

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 # coding=utf-8
 from flask import Flask
 
+class Config(object):
+    """配置类"""
+    DEBUG=True
 # 创建Flask应用程序实例
 app = Flask(__name__)
 
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
@@ -12,4 +16,4 @@ def index():
 
 if __name__ == '__main__':
     # 运行开发web服务器
-    app.run(debug=True)
+    app.run()
