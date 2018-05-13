634a6c7e1555b2f923066646a06c4961bae6cc72
Ceads <allenjo@163.com>
2018-05-13 21:16:07 +0800

    集成redis数据库

--- manage.py
+++ manage.py
@@ -1,15 +1,23 @@
 # coding=utf-8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
+
 
 class Config(object):
     """配置类"""
-    DEBUG=True
+    DEBUG = True
     # mysql数据库相关配置
     # 设置数据库的链接地址
-    SQLALCHEMY_DATABASE_URI="mysql://root:mysql@192.168.50.128:3306/ihome_sz08"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome_sz08"
     # 关闭追踪数据库的修改
-    SQLALCHEMY_TRACK_MODIFICATIONS =False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis数据库配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
 # 创建Flask应用程序实例
 app = Flask(__name__)
 
@@ -17,8 +25,14 @@ app.config.from_object(Config)
 
 # 创建SQLAlchemy对象
 db = SQLAlchemy(app)
+# 创建redis数据库链接对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+
 @app.route('/')
 def index():
+    # 测试redis
+    redis_store.set("name", "itcast")
     return 'index'
 
 
