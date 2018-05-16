ceb3b74deccbeebd11197c4ae0a75d3acfbd20fd
Ceads <allenjo@163.com>
2018-05-16 16:06:48 +0800

    以api接口的版本划分模块&api蓝图的创建

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -1,11 +1,15 @@
 # coding=utf-8
 import redis
-from flask import Flask, session
+from flask import Flask
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
 
 from config import config_dict
+from ihome.api_1_0 import api
+
+# 创建SQLAlchemy对象
+db = SQLAlchemy()
 
 # 工厂方法：
 def create_app(config_name):
@@ -17,8 +21,8 @@ def create_app(config_name):
 
     app.config.from_object(config_cls)
 
-    # 创建SQLAlchemy对象
-    db = SQLAlchemy(app)
+    # db对象进行app关联
+    db.init_app(app)
 
     # 创建redis数据库链接对象
     redis_store = redis.StrictRedis(host=config_cls.REDIS_HOST, port=config_cls.REDIS_PORT)
@@ -29,5 +33,8 @@ def create_app(config_name):
 
     # session信息存储
     Session(app)
+
+    # 3.注册蓝图对象(1&2在index.py)
+    app.register_blueprint(api)
 
     return app
--- manage.py
+++ manage.py
@@ -3,29 +3,17 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from ihome import create_app # , db
+from ihome import create_app, db
 
 # 需求：不修改业务逻辑的代码，只通过修改manage.py文件中的一句代码获取不同配置环境中的app
 app = create_app("development")
 # 创建Manager管理对象
-# manager = Manager(app)
-# Migrate(app, db)
+manager = Manager(app)
+Migrate(app, db)
 # # 添加迁移命令
-# manager.add_command("db", MigrateCommand)
-
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    # 测试redis
-    # redis_store.set("name", "itcast")
-
-    # 测试session存储
-    # session["name"] = "itheima"
-
-    return 'index'
-
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
     # 运行开发web服务器
-    app.run()
-    # manager.run()
+    # app.run()
+    manager.run()
