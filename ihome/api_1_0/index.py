cca8dd9071472496e66eb1da807c68c4a888ef73
Ceads <allenjo@163.com>
2018-05-17 23:19:55 +0800

    项目集成日志功能

--- ihome/api_1_0/index.py
+++ ihome/api_1_0/index.py
@@ -1,6 +1,8 @@
 # coding=utf-8
+import logging
 from . import api
 from ihome import redis_store
+from flask import current_app
 
 
 # 2.使用蓝图对象注册路由
@@ -11,5 +13,18 @@ def index():
 
     # 测试session存储
     # session["name"] = "itheima"
+
+    # 测试日志功能
+    # logging.fatal("Fatal Message")
+    # logging.error("Error Message")
+    # logging.warn("Warn Message")
+    # logging.info("Info Message")
+    # logging.debug("Debug Message")
+
+    # current_app.logger.fatal("Fatal Message")
+    # current_app.logger.error("Error Message")
+    # current_app.logger.warn("Warn Message")
+    # current_app.logger.info("Info Message")
+    # current_app.logger.debug("Debug Message")
 
     return 'index'
